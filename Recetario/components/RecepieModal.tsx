ad752c58fa13533d75a0a776a511efae509e3ddc
Mario Arias Hernandez <98143179+netrix4@users.noreply.github.com>
2025-01-10 23:57:41 -0800

    Update RecepieModal.tsx

--- Recetario/components/RecepieModal.tsx
+++ Recetario/components/RecepieModal.tsx
@@ -7,24 +7,60 @@ import {
   Button,
   View,
   Image,
+  ScrollView,
 } from "react-native";
+
+interface IDish {
+  Name: string;
+  Image: string;
+  Ingredients: string[];
+  Recipe: string[];
+}
 
 interface IPropsRecipeModal {
   onHidePress: () => void;
   isModalVisible: boolean;
+  singleDish: IDish;
 }
 
 const RecepieModal: React.FC<IPropsRecipeModal> = ({
   onHidePress,
   isModalVisible,
+  singleDish,
 }) => {
+  // console.log(singleDish);
+  function arrayOrString(){
+    
+    return (singleDish.Ingredients === typeof Array? singleDish.Ingredients.toString(): singleDish.Ingredients)
+  }
   return (
     <Modal visible={isModalVisible} animationType="slide">
       <SafeAreaView>
         <View style={styles.mainContainer}>
-          <Image source={require("@/assets/images/Pizza.png")} />
-          <Text>Insert recepie info here</Text>
-          <Button title="Hide Modal" onPress={onHidePress}></Button>
+          <View style={styles.dishesContainer}>
+            <View style={styles.propertiesWrapper}>
+
+            {/* <Image source={require("@/assets/images/Pizza.png")} /> */}
+            <Image style={styles.imageStyle} source={{uri: singleDish.Image}} />
+            {/* <ScrollView style={styles.scrollStyle}> */}
+
+              <View style={styles.propertiesContainer}>
+                <Text style={styles.propertiesTitle}>{singleDish.Name}</Text>
+              </View>
+              <View style={styles.propertiesContainer}>
+                <Text style={styles.propertiesText}>{arrayOrString()}</Text>
+              </View>
+              {/* <View style={styles.propertiesContainer}>
+                <Text style={styles.propertiesText}>{singleDish.Ingredients.toString()}</Text>
+              </View> */}
+              <View style={styles.propertiesContainer}>
+                <Text style={styles.propertiesText}>{singleDish.Recipe.toString()}</Text>
+              </View>
+            {/* </ScrollView> */}
+
+            </View>
+            <Button title="Hide Recepie" onPress={onHidePress}></Button>
+          </View>
         </View>
       </SafeAreaView>
     </Modal>
@@ -41,6 +77,42 @@ const styles = StyleSheet.create({
     alignItems: "center",
     gap: 8,
   },
+  dishesContainer: {
+    // height:725,
+    padding:'2%',
+    backgroundColor: "#ebecee",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  propertiesWrapper:{
+    height: 'auto',
+    padding:'2%',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+  propertiesContainer:{
+    backgroundColor: 'white',
+    marginVertical: '1%',
+    padding: '2%',
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  scrollStyle:{
+    height: 5
+  },
+  propertiesText:{
+    fontSize:15,
+  },
+  propertiesTitle:{
+    fontWeight: "bold",
+    fontSize:15,
+  },
+  imageStyle:{
+    width:350,
+    height:350
+  }
 });
 
 export { RecepieModal };
